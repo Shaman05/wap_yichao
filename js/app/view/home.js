ce8e1c2f518640571e6b414f7c448700417df5c3
陈超 <chenchao@upg.cn>
2014-09-01 17:17:51 +0800

    home page

--- js/app/view/home.js
+++ js/app/view/home.js
@@ -5,7 +5,11 @@
  * Time: 下午3:32
  */
 
-define(['app/view/baseView', 'app/service/api'], function(baseView, model){
+define([
+  'app/view/baseView',
+  'text!app/template/goods/ajax_product_list.html',
+  'app/service/api'
+], function(baseView, goodsListTpl, model){
 
   "use strict";
 
@@ -14,7 +18,26 @@ define(['app/view/baseView', 'app/service/api'], function(baseView, model){
       id: 'home-page',
       model: new model,
       ready: function(){
-        //
+        $("#jSlider").slider({
+          direction: "left",
+          height: 150
+        });
+      },
+      getGoodsList: function(){
+        //框架配镜 GoodsTypeID: 101
+        this.model.goodsList(101, page, function(d){
+          var renderFn = _.artTemplate.compile(goodsListTpl);
+          $('#proWapper_1').html(renderFn({
+            list: d.data
+          }));
+        });
+        //隐形眼瞳 GoodsTypeID: 102
+        this.model.goodsList(102, page, function(d){
+          var renderFn = _.artTemplate.compile(goodsListTpl);
+          $('#proWapper_2').html(renderFn({
+            list: d.data
+          }));
+        });
       }
     })
   );
