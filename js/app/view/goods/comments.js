c0baf3a06eb7516ab80c90e0d7abbad000b7ff9c
devin chen <shaman05@me.com>
2014-09-17 21:19:50 +0800

    goods

--- js/app/view/goods/comments.js
+++ js/app/view/goods/comments.js
@@ -2,21 +2,23 @@
 
 define([
   'app/view/baseView',
-  'text!app/template/goods/ajax_comments.html',
+  'text!app/template/goods/ajax_comments_list.html',
   'app/service/api'
 ], function(baseView, commentsTpl, api){
-
-  var page = 1;
 
   return Backbone.View.extend(
     $.extend(baseView, {
       id: 'comments-page',
       model: new api,
       ready: function(data){
-        this.model.goodsComments(data.GoodsID, page, function(d){
+        if(!data.PageIndex){
+          data.PageIndex = 1;
+        }
+        this.model.goodsComments(data.GoodsID, data.PageIndex, function(d){
           var renderFn = _.artTemplate.compile(commentsTpl);
           $('#commentsWrap').html(renderFn({
-            list: d.data
+            list: d.data,
+            GoodsID: data.GoodsID
           }));
         });
       }
--- js/app/view/goods/product_info.js
+++ js/app/view/goods/product_info.js
@@ -16,19 +16,22 @@ define([
       model: new api,
       ready: function(data){
         this.imgSlider(data);
-
         //商品信息
         this.model.goodsInfo(data.GoodsID, function(d){
           var renderFn = _.artTemplate.compile(infoTpl);
           $('#proInfoBox').html(renderFn(d.data[0]));
+          $('#CommentTimes').text(d.data[0]['CommentTimes']);
         });
+        this.getComments(data);
+      },
+      getComments: function(data){
         //评论
-        /*this.model.goodsComments(data.GoodsID, page, function(d){
+        this.model.goodsComments(data.GoodsID, page, function(d){
           var renderFn = _.artTemplate.compile(commentsTpl);
           $('#commentsWrap').html(renderFn({
             list: d.data
           }));
-        });*/
+        });
       },
       imgSlider: function(data){
         this.model.goodsInfoSlider(data.GoodsID, function(d){
