ab8ff514acc49192f9f315f791502a712803a471
devin chen <shaman05@me.com>
2014-09-21 00:12:13 +0800

    brand

--- js/app/view/goods/brand.js
+++ js/app/view/goods/brand.js
@@ -2,13 +2,13 @@
 
 define([
   'app/view/baseView',
-  'text!app/template/goods/ajax_product_list.html',
+  'text!app/template/goods/ajax_brandList.html',
   'app/service/api'
 ], function(baseView, tpl, model){
 
   return Backbone.View.extend(
     $.extend(baseView, {
-      id: 'home-page',
+      id: 'brand-page',
       model: new model,
       ready: function(data){
         if(!data.PageIndex){
@@ -21,8 +21,12 @@ define([
       },
       getBrandList: function(data){
         this.model.goodsBrand(data.GoodsTypeID, data.PageIndex, function(d){
+          var $box = $('#brandBox');
+          if(d.data && d.data.length > 0){
+            $box.css('background', '#000000');
+          }
           var renderFn = _.artTemplate.compile(tpl);
-          $('#brandBox').html(renderFn({
+          $box.html(renderFn({
             list: d.data,
             goodsTypeId: data.GoodsTypeID,
             pageIndex: data.PageIndex,
