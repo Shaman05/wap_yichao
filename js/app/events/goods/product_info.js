0ddc0f55f045c1ac50f8a6b3f89c49f7419937e6
devin chen <shaman05@me.com>
2014-09-20 23:13:20 +0800

    goods info

--- js/app/events/goods/product_info.js
+++ js/app/events/goods/product_info.js
@@ -12,6 +12,17 @@ define(['events'], function(events){
   return function(view, service){
     events.init();
     $(document)
+      .on('click', '[data-glassFrame] a, [data-glassLens] a, [data-moreLens] a', function(){
+        var $this = $(this);
+        if(!$this.hasClass('lastA')){
+          $this.addClass('focus').siblings().removeClass('focus');
+        }
+      })
+      //更多镜片
+      .on('click', '#selectMore', function(){
+        var $e = $('#moreSelectWrap');
+        $e.toggle($e[0].style.display == "none");
+      })
       //加入购物车
       .on('click', '#addToCartBtn', function(){
         service.addToCart({
