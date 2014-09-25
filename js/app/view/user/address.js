cdb75f6fe8ee8eec483cacdbcc01fc6e9996b6e5
devin chen <shaman05@me.com>
2014-09-26 00:40:03 +0800

    address

--- js/app/view/user/address.js
+++ js/app/view/user/address.js
@@ -1,11 +1,23 @@
 "use strict";
 
-define(['app/view/baseView'], function(baseView){
+define([
+  'app/view/baseView',
+  'text!app/template/user/ajax_address_list.html',
+  'app/service/api'
+], function(baseView, tpl, model){
 
   return Backbone.View.extend(
     $.extend(baseView, {
-      id: 'home-page',
-      model: null
+      id: 'address-page',
+      model: new model,
+      ready: function(data){
+        this.model.getMemberAddress('', '', function(d){
+          var renderFn = _.artTemplate.compile(tpl);
+          $('#addressListBox').html(renderFn({
+            list: d.data
+          }));
+        });
+      }
     })
   );
 
