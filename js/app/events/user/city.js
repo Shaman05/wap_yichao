ddb3d71db1e60ee3f459a9fbcf2a791f09dc6f8c
devin chen <shaman05@me.com>
2014-10-14 23:41:19 +0800

    city

--- js/app/events/user/city.js
+++ js/app/events/user/city.js
@@ -12,8 +12,14 @@ define(['events'], function(events){
   return function(view, service){
     events.init();
     $(document)
-      .on('click', '.data-province, .data-city', function(){
-        $(this).next().toggle();
+      .on('click', '.data-province, .data-city, .data-area', function(){
+        var hasSub = $(this).next('ul');
+        if(hasSub.size() > 0){
+          $(this).next().toggle();
+        }else{
+          window.sessionStorage.setItem('selectedCity', '{"foo": "foo"}');
+          util.toPage('user.addAddress');
+        }
       });
   };
 
