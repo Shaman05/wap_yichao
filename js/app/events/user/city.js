e047213cb67f051df10d8b2ee2eca5021e8a3f50
devin chen <shaman05@me.com>
2014-10-16 23:03:27 +0800

    city

--- js/app/events/user/city.js
+++ js/app/events/user/city.js
@@ -13,13 +13,39 @@ define(['events'], function(events){
     events.init();
     $(document)
       .on('click', '.data-province, .data-city, .data-area', function(){
-        var hasSub = $(this).next('ul');
-        if(hasSub.size() > 0){
-          $(this).next().toggle();
+        var $this = $(this);
+        var $nextUl = $this.next('ul');
+        var ParentID = $this.attr('data-ParentID');
+        var realLevel = parseInt($this.attr('data-level'), 10) + 1;
+        if($this.data('loaded') != '1'){
+          service.getArea(realLevel, ParentID, function(d){
+            var data = d.data;
+            if(d.status == "0"){
+              alert(d.message);
+              return;
+            }
+            if(d.status == "1" && data && data.length > 0){
+              $this.data('loaded', 1);
+              view.renderList(data, realLevel, function(html){
+                $nextUl.html(html);
+              });
+            }else{
+              window.sessionStorage.setItem('selectedCity', '{"text": "杭州市西湖区火车站", "provinceID": "1", "cityID": "2", "areaID": "3"}');
+              util.toPage('user.addAddress');
+            }
+          });
         }else{
-          window.sessionStorage.setItem('selectedCity', '{"text": "杭州市西湖区火车站", "provinceID": "1", "cityID": "2", "areaID": "3"}');
-          util.toPage('user.addAddress');
+          $nextUl.toggle();
         }
+        /*view.getArea(level, pid, function(){
+          var hasSub = $this.next('ul');
+          if(hasSub.size() > 0){
+            $this.next().toggle();
+          }else{
+            window.sessionStorage.setItem('selectedCity', '{"text": "杭州市西湖区火车站", "provinceID": "1", "cityID": "2", "areaID": "3"}');
+            util.toPage('user.addAddress');
+          }
+        });*/
       });
   };
 
--- js/app/view/user/city.js
+++ js/app/view/user/city.js
@@ -11,9 +11,33 @@ define([
       id: 'selectAddress-page',
       model: new model,
       ready: function(){
-        this.model.getArea(3, '', function(d){
-
+        this.getArea(2, '');
+      },
+      getArea: function(AreaLevel, ParentID){
+        var _this = this;
+        var realLevel = parseInt(AreaLevel, 10) + 1;
+        this.model.getArea(realLevel, ParentID, function(d){
+          var data = d.data;
+          if(d.status == "1"){
+            _this.renderList(data, realLevel);
+          }
         });
+      },
+      renderList: function(data, realLevel, callback){
+        var len = data.length;
+        var html = '';
+        if(data && len > 0){
+          for(var i = 0; i < len; i++){
+            var item = data[i];
+            html +=  '<li><a class="data-province" href="javascript:" data-level="' + item['AreaLevel'] +
+              '" data-ParentID="' + item['AreaID'] + '">' + item['AreaName'] + '</a><ul></ul></li>';
+          }
+          if(realLevel == 3){
+            $('.cityList').html(html);
+          }else{
+            callback && callback(html);
+          }
+        }
       }
     })
   );
