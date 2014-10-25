20fa73c22a5e66759dc0e2d5d0260f2b710c4a12
unknown <shaman@shaman-PC.(none)>
2014-10-25 23:00:10 +0800

    goods info

--- js/app/events/goods/product_info.js
+++ js/app/events/goods/product_info.js
@@ -36,26 +36,30 @@ define(['events'], function(events){
       })
       //加入购物车
       .on('click', '#addToCartBtn', function(){
+        var $glassframeSelected = $('[data-glassframe]').find('.focus');
+        var skuName = $glassframeSelected.text();
+        var skuCode = $glassframeSelected.attr('data-itemCode');
+        var LensesID = $('[data-glasslens]').find('.focus').attr('data-goodsid');
         service.addToCart({
             UserTicket: "23232"  //未登录用户票据 待确认
           , GoodsID: $('#GoodsID').val()  //商品ID
           , GoodsName: $('#GoodsName').val()  //商品名称
           , GoodsTypeID: $('#GoodsTypeID').val()  //商品类型
-          , SkuName: "23"  //商品SKU名称 待确认
-          , SkuCode: "23"  //商品SKU代号 待确认
+          , SkuName: skuName  //商品SKU名称
+          , SkuCode: skuCode  //商品SKU代号
           , Qty: 1  //商品数量
           , IsChangeQty: ""  //是否允许更新数量 待确认
           , SalePrice: parseFloat($('#SalePrice').val())  //销售价格
           , OriginalPrice: $('#OriginalPrice').val()  //原售价
-          , LevelDiscount: $('#LevelDiscount').val()  //会员等级折扣额 待确认
+          , LevelDiscount: $('#LevelDiscount').val()  //会员等级折扣额
           , PromotionID: $('#PromotionID').val()  //促销活动ID
           , Point: $('#Point').val()  //赠送积分
           , Weight: $('#Weight').val()  //重量（单位：克）
-          , ParentID: $('#ParentID').val() || 0 //购物车商品父级ID：0=无父级；>0 且IsChild=False为父级； 待确认
+          , ParentID: $('#ParentID').val() || 0 //购物车商品父级ID：0=无父级；>0 且IsChild=False为父级；
           , IsChild: "23"  //是否为子级 待确认
           , Expired: "23"  //未结算过期时间 待确认
-          , PrescriptionsID: "23"   //验光单ID
-          , LensesID: "23"  //眼镜架配的镜片商品ID
+          , PrescriptionsID: ""   //验光单ID 选验光单时追加
+          , LensesID: LensesID  //眼镜架配的镜片商品ID
           , IsOnlyFrames: "23"  //待确认
           , IsCustomzied: "23"  //待确认
           , GoodsBrandID: "23"  //待确认
